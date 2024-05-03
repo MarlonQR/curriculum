ad4bc86992a7e3580f217031b4a2580c68ded62a
MarlonQR <mquinterorin@ufpso.edu.co>
2024-05-03 14:36:10 -0500

    learning js

---
+++ js/functions.js
@@ -0,0 +1,86 @@
+// // 4 Formas de impresion
+// alert("HOLA DESDE UN ARCHIVO EXTERNO")
+// console.log("HOLA DESDE LA CONSOLA");
+// document.write("HOLA DESDE LA INTERFAZ")
+// // Swal.fire("HOLA DESDE SWETTALERT");
+// Swal.fire({
+//     title: "The Internet?",
+//     text: "That thing is still around?",
+//     icon: "error"
+//   });
+
+// VARIABLES Y TIPOS DE DATOS
+const pi = 3.14; //Const: se aplica a valores que no se van a cambiar (DECIMAL) 
+const pul = 2.54;
+var name = "Marlon Quintero"; //var: variables globales (STRING)
+let edad = 18;//(ENTERO)
+var ver = true; //(BOOLEAN)
+var fal = false;
+
+
+//OPERADORES BASICOS
+var suma = 5 + 2;
+console.log(suma);
+var var_uno = 10;
+var var_dos = 5;
+console.log(var_uno - var_dos);
+console.log(var_uno * var_dos);
+console.log(var_uno / var_dos);
+console.log(var_uno % var_dos);
+
+
+//OPERADORES DE COMPARACION
+// = Asignacion
+// == Comparacion (valor)
+// === estrictamente comparado (valor y el tipo de dato )
+// >
+// >=
+// <=
+// <
+// <>
+
+console.log("Mi edad es : " + edad);
+
+
+//EVENTO
+
+function load_page(){
+    Swal.fire({
+        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
+        // imageHeight: 1500,
+        imageAlt: "A tall image",
+        showConfirmButton:false,
+        timer: 2000
+      });
+}
+
+function send_form(){
+    let name      = document.getElementById("name").value;
+    let last_name = document.getElementById("last_name").value;
+    
+    if(name.length == 0 || last_name.length == 0){
+        Swal.fire({
+            title: "Cajas de texto vacias",
+            text: "Alguna de las cajas de texto estan vacias",
+            icon: "error"
+        });
+        if(name.length == 0){
+            document.getElementById("name").style.border= "2px solid red"
+        }
+        else{
+            document.getElementById("name").style.border= "2px solid green"
+        }
+        if(last_name.length ==0){
+            document.getElementById("last_name").style.border= "2px solid red"
+        }
+        else{
+            document.getElementById("last_name").style.border= "2px solid green"
+        }
+
+    }
+    else{
+        document.getElementById("print").innerText = "Su nombre es " +name+"   "+last_name;
+
+    }
+
+}
